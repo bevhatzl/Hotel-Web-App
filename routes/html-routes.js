8b55b33414b2f35439aa8dd0540c54bc99c2f77a
Chee Quee <mitchellcq@gmail.com>
2020-11-19 12:49:20 +1100

    wrote out html routes for the different pages, i added and isAdmin.js file for authentication but it doesnt really do anything yet

---
+++ routes/html-routes.js
@@ -0,0 +1,34 @@
+// Requiring path to so we can use relative routes to our HTML files
+const path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAdmin = require("../config/middleware/isAdmin");
+
+module.exports = function (app) {
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/login", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/bookings");
+        }
+        res.sendFile(path.join(__dirname, "../public/login.html"));
+    });
+
+    // Here we've add our isAuthenticated middleware to this route.
+    // If a user who is not logged in tries to access this route they will be redirected to the signup page
+    app.get("/bookings", isAuthenticated, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/bookings.html"));
+    });
+
+    app.get("/signup", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
+
+    app.get("/admin", isAdmin, (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/admin.html"));
+    })
+};
