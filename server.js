debd6f324e0fd421ece53a31b3477611c02d2cd4
Cheryl <cheryljfowlie@me.com>
2020-11-19 19:46:11 +1100

    Add routing

---
+++ server.js
@@ -0,0 +1,24 @@
+// Requiring necessary npm packages
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+var app = express();
+const PORT = process.env.PORT || 8000;
+
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Code for routing
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
