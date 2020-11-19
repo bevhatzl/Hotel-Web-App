7ffd2980d52a68256ef8415fe5464fcc589f365a
Myles Burt <myles_burt@rocketmail.com>
2020-11-19 18:29:21 +1030

    don't commit this

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,7 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
+// Requiring our custom middleware for checking if a user is logged in blah blah
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 const isAdmin = require("../config/middleware/isAdmin");
 
