a623ab98abcbe44547be25735d64fe88d4dcbf93
Chee Quee <mitchellcq@gmail.com>
2020-11-23 19:12:49 +1100

    added signup html and javascript, basic form works so far but not linked to anything properly

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,4 +6,8 @@ module.exports = function (app) {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+
+    app.get("/signup", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+    });
 };
